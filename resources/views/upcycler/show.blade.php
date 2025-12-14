836917c1f8baa318f3aa3eefa2ce40431b5526fa
clintoy18 <aicsalonzo@gmail.com>
2025-12-14 09:13:09 +0800

    add contactnumber on appointment viewing upcycler

--- resources/views/upcycler/show.blade.php
+++ resources/views/upcycler/show.blade.php
@@ -102,7 +102,6 @@
                                         {{ $appointment->user->lname }}, {{ $appointment->user->fname }}</p>
                                 </div>
                             </div>
-
                             <div class="flex items-center space-x-3">
                                 <svg class="w-5 h-5 text-[#B59F84]" fill="none" stroke="currentColor"
                                     viewBox="0 0 24 24">
@@ -114,6 +113,19 @@
                                     <p class="text-sm text-gray-500 dark:text-gray-400">Email</p>
                                     <p class="font-medium text-gray-900 dark:text-gray-100">
                                         {{ $appointment->user->email }}</p>
+                                </div>
+                            </div>
+                            <div class="flex items-center space-x-3">
+                                <svg class="w-5 h-5 text-[#B59F84]" fill="none" stroke="currentColor"
+                                    viewBox="0 0 24 24">
+                                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                        d="M3 5a2 2 0 012-2h3.28a1 1 0 01.948.684l1.498 4.493a1 1 0 01-.502 1.21l-2.257 1.13a11.042 11.042 0 005.516 5.516l1.13-2.257a1 1 0 011.21-.502l4.493 1.498a1 1 0 01.684.949V19a2 2 0 01-2 2h-1C9.716 21 3 14.284 3 6V5z">
+                                    </path>
+                                </svg>
+                                <div>
+                                    <p class="text-sm text-gray-500 dark:text-gray-400">Contact Number</p>
+                                    <p class="font-medium text-gray-900 dark:text-gray-100">
+                                        {{ $appointment->contactnumber }}</p>
                                 </div>
                             </div>
                         </div>
